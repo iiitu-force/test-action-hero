04d1afe99ad657674d5b052e24624cb40af60ac8
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-02-15 23:48:42 +0530

    User model successfully created

--- initializers/user.js
+++ initializers/user.js
@@ -1,55 +1,24 @@
 // const mongoose = require('mongoose');
 const {Initializer, api } = require('actionhero');
 const mongoose = require('mongoose'),
-passport = require('passport'),
-localStrategy = require('passport-local'),
-    passportLocalMongoose = require('passport-local-mongoose');
+      passport = require('passport'),
+      localStrategy = require('passport-local'),
+      passportLocalMongoose = require('passport-local-mongoose');
     // const User = require('/home/ayush/IIITU/alumni-website/models/user.js');
 
 mongoose.connect("mongodb://localhost/alumni_db");
 
 
-// const userSchema = new mongoose.Schema({
-//   rollNo : String ,
-//   firstName : String ,
-//   lastName : String ,
-//   branch :String ,
-//   password :String ,
-//   email : String 
-// });
-
-// userSchema.plugin(passportLocalMongoose);
-
-// const User = mongoose.model("User", userSchema);
-
-
-var users=mongoose.model('users', {
-  rollNo : String ,
+var userSchema = new mongoose.Schema({
   firstName : String ,
   lastName : String ,
+  rollNo : String ,
+  email : String ,
   branch :String ,
-  password :String ,
-  email : String 
-  // hostel:{
-  //   type:String
-  // },
-  // lib:{
-  //   type:Number
-  // },
-  // acad:{
-  //   type:String
-  // },
-  // acadv:{
-  //   type:Boolean
-  // },
-  // hostelv:{
-  //   type:Boolean
-  // }
+  password :String 
 });
 
-
-
-
+var User = mongoose.model("User", userSchema);
 
 module.exports = class Users extends Initializer {
   constructor() {
@@ -63,70 +32,25 @@ module.exports = class Users extends Initializer {
   async initialize() {
     api.users = {
       signup: async (firstName, lastName, password, branch, email, rollNo) => {
-          console.log("in signup");
-          try{
-            console.log("In try");
-            var regn = new users({
-              _id : new ObjectID(),
-              firstName : firstName,
+            var newUser = User.create({
+            firstName : firstName,
             lastName : lastName,
             password : password,
             branch : branch,
             rollNo : rollNo,
             email : email
-              // acad:req.body.Acadmic,acadv:false,hostelv:false
-            });
-            regn.save().then((doc) => {
-              console.log("success");
-              // res.send(doc);
-            }, (err) => {
-               throw err;
-            });
-          //   const newUser = User.create({
-          //     _id:new ObjectID(),
-          //   firstName : firstName,
-          //   lastName : lastName,
-          //   password : password,
-          //   branch : branch,
-          //   rollNo : rollNo,
-          //   email : email
-          // });
-
-          // newUser.save();
-
-
-          //  async function(err, newUser){
-          //   console.log("in function");
-          //   if(err)  {
-          //     console.log("in error");
-          //     console.log(err);
-          //     throw new Error(err);
-          //   }
-          //   else{
-          //     console.log("save");
-          //     console.log(newUser);
-
-              // try{
-              // await newUser.save();
-              // // }
-              // catch(err)
-              // {
-              //   throw new Error(err);
-              // }
-              // console.log("saved user");
-
-
-            }
-          // })
-        catch(err)
-        {
-            return {error: err, token: null};
-        }
-        finally
-        {
-          return {error: null, token: "success"};
-
-        }
+          }, function(err, foundUser){
+              if(err){  
+                console.log(err);
+                return {error: err, token: null};
+              }
+              else{
+                foundUser.save();
+                console.log("User created successfully");
+              }
+          });
+          
+         return {error: null, token: "success"};
       }
     }
   }
